21ec8dd958a60b525e69bc94361c8cdbfde0b3ce
Viktor Ceder <vceder@kth.se>
2018-05-07 21:29:49 +0200

    update

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/viktorceder.se',
+    assetsPublicPath: 'techpriestv.github.io/viktorceder.se',
 
     /**
      * Source Maps
