38c3ceed9a252e4ce7fc96c25b6e6181ca8d27ad
Viktor Ceder <vceder@kth.se>
2019-02-22 15:13:03 +0100

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <!-- <img src="./assets/VEnterpriseRoundNoText.png"> -->
     <p>Hello World</p>
+    <router-link to="/projects">Projects</router-link>
     <router-view/>
+    <p>Technical problems please hold by</p>
   </div>
 </template>
 
@@ -13,12 +15,20 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .scrolling-wrapper-flex {
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: auto;
+  }
+  .card {
+    flex: 0 0 auto;
+  }
 </style>
